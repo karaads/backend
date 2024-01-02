6a7e7ef757d1d1492db221ac989d13ce3c8ab91f
Uhembenelson <uhembenelson@gmail.com>
2024-01-02 08:41:16 +0100

    stop trx

--- controllers/User/VerifyBankAccount.js
+++ controllers/User/VerifyBankAccount.js
@@ -101,6 +101,18 @@ export const verifyBankAccount =  async( req, res)=>{
 
 
  export  const verifyTransfer = async (req, res)=>{ 
+
+
+  const data = {
+    status:true,
+    message:"Transfer is Unavailable at the moment, we are working hard to bring this update to you soon"
+  
+}
+return  res.send(data)
+
+
+
+
   const findUser = await User.findOne({apiKey: req.body.apiKey})
   
  
