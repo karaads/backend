734b5af6771b48c2770c9749804379663722fcc6
Uhembenelson <uhembenelson@gmail.com>
2024-01-03 16:41:51 +0100

    network erroor

--- controllers/User/Signup.js
+++ controllers/User/Signup.js
@@ -5,56 +5,62 @@ import { hashPassword, welcomeEmail, CreateTransaction } from "../../utils/Index
 //import { v4 as uuidv4 } from 'uuid';
 //uuidv4();
 export const Signup = async (req, res) => {
-   const code =  Math.random().toString(36).substr(2, 6);
+
+    res.status(201).send({ 
+        data,
+        msg: "There was a network error please try again later"
+
+    });
+//    const code =  Math.random().toString(36).substr(2, 6);
   
-    const { firstName, lastName, email, address, city, state, zip, phoneNumber, dateOfBirth, userType, vendorCode, fullname, pin } = req.body;
-    //math.random
-    const apiKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    const password = await hashPassword(req.body.password);
-    const user = new User({pin, fullname, firstName, lastName, email, password, address, city, state, zip, phoneNumber, dateOfBirth, apiKey, userType, vendorCode, referalCode:code});
+//     const { firstName, lastName, email, address, city, state, zip, phoneNumber, dateOfBirth, userType, vendorCode, fullname, pin } = req.body;
+//     //math.random
+//     const apiKey = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+//     const password = await hashPassword(req.body.password);
+//     const user = new User({pin, fullname, firstName, lastName, email, password, address, city, state, zip, phoneNumber, dateOfBirth, apiKey, userType, vendorCode, referalCode:code});
 
-    try {
-        //check if user with email already exists
-        const refcode = await User.findOne({referalCode: req.body.referalCode})
-        const userExists = await User.findOne({ phoneNumber});
-        if (userExists) {
-            console.log("working",userExists)
-          return res.status(200).send({ 
-                msg: "User already exist"
+//     try {
+//         //check if user with email already exists
+//         const refcode = await User.findOne({referalCode: req.body.referalCode})
+//         const userExists = await User.findOne({ phoneNumber});
+//         if (userExists) {
+//             console.log("working",userExists)
+//           return res.status(200).send({ 
+//                 msg: "User already exist"
     
-            });
+//             });
 
 
-            // return res.status(409).send({ msg: "User already exist" });
-        }
-        //save user to database
-        await user.save();
-        if(refcode){
-            const updateBalance = parseInt(refcode.balance ) + parseInt(20)
-            await User.updateMany({_id : refcode._id},{$set:{ balance : updateBalance}})
-            const data = {
-                id:refcode._id,
-                amount: 20,
-                type:"commission",
-                description:`Referral Bonus of 20 Naira`,
+//             // return res.status(409).send({ msg: "User already exist" });
+//         }
+//         //save user to database
+//         await user.save();
+//         if(refcode){
+//             const updateBalance = parseInt(refcode.balance ) + parseInt(20)
+//             await User.updateMany({_id : refcode._id},{$set:{ balance : updateBalance}})
+//             const data = {
+//                 id:refcode._id,
+//                 amount: 20,
+//                 type:"commission",
+//                 description:`Referral Bonus of 20 Naira`,
                 
-            }
-          const addTransaction = CreateTransaction(data)
-          addTransaction
-        }
+//             }
+//           const addTransaction = CreateTransaction(data)
+//           addTransaction
+//         }
 
 
 
-        //send welcome email
-        // welcomeEmail(user.email, user.firstName);
-        const data = user
-        res.status(201).send({ 
-            data,
-            msg: "Signup successful"
+//         //send welcome email
+//         // welcomeEmail(user.email, user.firstName);
+//         const data = user
+//         res.status(201).send({ 
+//             data,
+//             msg: "Signup successful"
 
-        });
-    } catch (error) {
-        console.log("there was an  error", error);
-        res.status(400).send(error);
-    }
+//         });
+//     } catch (error) {
+//         console.log("there was an  error", error);
+//         res.status(400).send(error);
+//     }
 }
